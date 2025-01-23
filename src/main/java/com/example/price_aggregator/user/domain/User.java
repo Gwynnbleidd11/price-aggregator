f2f56e1d009b85d826fdfc1193df46808f3c331f
Patryk Krzyżak <p.krzyzak1986@gmail.com>
2025-01-23 11:33:42 +0100

    Spring security implemented and working

--- src/main/java/com/example/price_aggregator/user/domain/User.java
+++ src/main/java/com/example/price_aggregator/user/domain/User.java
@@ -1,22 +1,25 @@
 package com.example.price_aggregator.user.domain;
 
 import jakarta.persistence.*;
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
+import lombok.*;
 import org.hibernate.annotations.UuidGenerator;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
 
 import java.time.LocalDate;
+import java.util.Collection;
+import java.util.List;
 import java.util.UUID;
 
 @Getter
 @Setter
+@Builder
 @NoArgsConstructor
 @AllArgsConstructor
 @Entity
 @Table(name = "users")
-public class User {
+public class User implements UserDetails {
 
     @Id
     @UuidGenerator
@@ -31,4 +34,42 @@ public class User {
     private String email;
 
     private String password;
+
+    @Enumerated(EnumType.STRING)
+    private Role role;
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return List.of(new SimpleGrantedAuthority(role.name()));
+    }
+
+    @Override
+    public String getPassword() { //this is actually not needed if our password field is called "password" and we have lombok.
+        return password;            // Lombok generates that method for us
+    }
+
+    @Override
+    public String getUsername() {
+        return email;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
 }
--- src/main/java/com/example/price_aggregator/user/repository/UserRepository.java
+++ src/main/java/com/example/price_aggregator/user/repository/UserRepository.java
@@ -1,15 +1,17 @@
 package com.example.price_aggregator.user.repository;
 
 import com.example.price_aggregator.user.domain.User;
-import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
+import java.util.Optional;
 import java.util.UUID;
 
 @Repository
-public interface UserRepository extends CrudRepository<User, UUID> {
+public interface UserRepository extends JpaRepository<User, UUID> {
 
     @Override
     List<User> findAll();
+    Optional<User> findByEmail(String email);
 }
